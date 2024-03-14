45d5c621d2e39635879448fc529b7a88778fb602
sirotaiv <otaviorau@gmail.com>
2024-03-13 21:42:21 -0300

    Initial Commit

---
+++ src/test/java/net/thetome/library/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thetome.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
